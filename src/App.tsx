863df2f41bc3b1f82d6af61a42eeac9f60a35372
tkctkc <38493534+Takeshi-005@users.noreply.github.com>
2020-02-03 12:53:03 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,167 @@
 import React, { useReducer }from 'react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form'
-import { formReducer, initialState, State, FormName } from './reducer';
+import { formReducer, initialState, FormName } from './reducer';
 import Input from './components/input';
+import { GlobalStyle } from './globalStyle'
 
-const App: React.FC<{className?:string}> = props => {
+
+const ReducetrForm:React.FC = () => {
   const [state, dispatch] = useReducer(formReducer, {
     values:initialState
   });
-  const { register, handleSubmit, errors, clearError, watch, setValue }
-    = useForm<State>();
-  return <>
-    <div className={props.className}>
-      useReducerを使用したフォーム
-      <div className="form">
-        名前：
-        <Input
-          name="name"
-          value={state.values.name}
-          dispatch={dispatch}
-        />
-        </div>
-      <div className="form">
-        ふりがな：
-        <Input
-          name="kana"
-          value={state.values.kana}
-          dispatch={dispatch}
-        />
-        </div>
-      <div className="form">
-        電話番号：
-        <Input
-          name="tel"
-          value={state.values.tel}
-          dispatch={dispatch}
-        />
-        </div>
-      <div className="form">
-        メールアドレス：
-        <Input
-          name="email"
-          value={state.values.email}
-          dispatch={dispatch}
-        />
-        </div>
-      <div className="form">
-        住所：
-        <Input
-          name="address"
-          value={state.values.address}
-          dispatch={dispatch}
-        />
-        </div>
-        <div>
-          値：
-          {
-            Object.keys(state.values)
-            .map((key) => <li key={key}>{key}: {state.values[key as keyof FormName]}</li>)
-          }
-        </div>
+  return<>
+  useReducerを使用したフォーム
+  <div className="form">
+    名前：
+    <Input
+      name="name"
+      value={state.values.name}
+      dispatch={dispatch}
+    />
     </div>
-    <div className={props.className}>
-      react-hook-formを使用
+  <div className="form">
+    ふりがな：
+    <Input
+      name="kana"
+      value={state.values.kana}
+      dispatch={dispatch}
+    />
+    </div>
+  <div className="form">
+    電話番号：
+    <Input
+      name="tel"
+      value={state.values.tel}
+      dispatch={dispatch}
+    />
+    </div>
+  <div className="form">
+    メールアドレス：
+    <Input
+      name="email"
+      value={state.values.email}
+      dispatch={dispatch}
+    />
+    </div>
+  <div className="form">
+    住所：
+    <Input
+      name="address"
+      value={state.values.address}
+      dispatch={dispatch}
+    />
+    </div>
+    <div>
+      値：
+      {
+        Object.keys(state.values)
+        .map((key) => <li key={key}>{key}: {state.values[key as keyof FormName]}</li>)
+      }
+    </div>
+  </>
+}
+
+const ReactHookForm:React.FC = () => {
+    const { register, watch }
+    = useForm<FormName>();
+  return <>
+    react-hook-formを使用
+    <div className="form">
+      名前：
+      <Input
+        name="name"
+        refs={register()}
+      />
+      </div>
+    <div className="form">
+      ふりがな：
+      <Input
+        name="kana"
+        refs={register()}
+      />
+      </div>
+    <div className="form">
+      電話番号：
+      <Input
+        name="tel"
+        refs={register()}
+      />
+      </div>
+    <div className="form">
+      メールアドレス：
+      <Input
+        name="email"
+        refs={register()}
+      />
+      </div>
+    <div className="form">
+      住所：
+      <Input
+        name="address"
+        refs={register()}
+      />
+      </div>
+      <div>
+        値：
+        {
+          Object.keys(initialState)
+          .map((key) => <li key={key}>{key}: {watch(key)}</li>)
+        }
+      </div>
+  </>
+}
+
+const ReactHookValidForm:React.FC = () => {
+  const { register, handleSubmit, errors, clearError, watch, setValue } = useForm<FormName>();
+  const ERROR_TEXT = {
+    required: "入力してください",
+    kana: "ひらがなで入力してください",
+    tel: "正しい形式で入力してください"
+  }
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  })
+  return <>
+    react-hook-formを使用
+    <form onSubmit={onSubmit}>
       <div className="form">
         名前：
         <Input
           name="name"
-          refs={register()}
+          refs={register({
+            required: ERROR_TEXT.required
+          })}
         />
+        {errors.name && <p className="error-text">{errors.name.message}</p>}
         </div>
       <div className="form">
         ふりがな：
         <Input
           name="kana"
-          refs={register()}
+          refs={register({
+            required: ERROR_TEXT.required,
+            pattern: {
+              value: /^[ぁ-んー　]+$/,
+              message: ERROR_TEXT.kana
+            }
+          })}
         />
+        {errors.kana && <p className="error-text">{errors.kana.message}</p>}
         </div>
       <div className="form">
         電話番号：
         <Input
           name="tel"
-          refs={register()}
+          refs={register({
+            required: ERROR_TEXT.required,
+            pattern: {
+              value: /\d{9}/,
+              message:ERROR_TEXT.tel
+            }
+          })}
         />
+        {errors.tel && <p className="error-text">{errors.tel.message}</p>}
         </div>
       <div className="form">
         メールアドレス：
@@ -105,16 +184,42 @@ const App: React.FC<{className?:string}> = props => {
             .map((key) => <li key={key}>{key}: {watch(key)}</li>)
           }
         </div>
+        <input type="submit" value="送信する"/>
+      </form>
+  </>
+}
+
+
+const App: React.FC<{className?:string}> = props => {
+  return <>
+    <div className={props.className}>
+      <ReducetrForm />
     </div>
+    <div className={props.className}>
+      <ReactHookForm />
+    </div>
+    <div className={props.className}>
+      <ReactHookValidForm />
+    </div>
+    <GlobalStyle/>
   </>
 }
 
 export default styled(App)`
-  width: 800px;
+  max-width: 980px;
   margin: 0 auto;
-  > .form {
+  border: 1px solid #ccc;
+  padding: 10px;
+  & + & {
+    margin-top: 16px;
+  }
+  .form {
     padding: 10px;
     border-bottom: 1px solid #ccc;
+
+    .error-text {
+      color: #f00;
+    }
   }
 
 `

ebb7064d924bc1b609adb2b92138d08799db8320
tkctkc <38493534+Takeshi-005@users.noreply.github.com>
2020-02-06 16:16:47 +0900

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import styled from 'styled-components';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { GlobalStyle } from './globalStyle'
+
+import ReducerForm from './components/ReducerForm';
+import ReactHookForm from './components/ReactHookForm';
+import ReactHookValidForm from './components/ReactHookValidForm';
+
+
+const App: React.FC<{className?:string}> = props => {
+  return <>
+    <div className={props.className}>
+      <ReducerForm />
+    </div>
+    <div className={props.className}>
+      <ReactHookForm />
+    </div>
+    <div className={props.className}>
+      <ReactHookValidForm />
+    </div>
+    <GlobalStyle/>
+  </>
+}
+
+export default styled(App)`
+  max-width: 980px;
+  margin: 0 auto;
+  border: 1px solid #ccc;
+  padding: 10px;
+  & + & {
+    margin-top: 16px;
+  }
+  .form {
+    padding: 10px;
+    border-bottom: 1px solid #ccc;
+  }
+
+`
